cd5c13612d5dbacd13f277368d6bcf2818155a75
Adam <akhoshnaw100@gmail.com>
2026-03-29 21:13:08 -0700

    Logistic Regression

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 # for Naive Bayes
-from sklearn.naive_bayes import MultinomialNB
+#from sklearn.naive_bayes import MultinomialNB
+
+# for Logistic Regression
+from sklearn.linear_model import LogisticRegression
+
 from sklearn import metrics
+
+
 
 df = pd.read_csv('emails.csv')
 
 print(df.head()) # show first 5 rows
-print(df.shape)  # show 5728 emails and 2 columns
+print(df.shape)  # show (rows, columns)
 print(df['spam'].value_counts()) # 0 = legit | 1 = spam
 
-x = df['text'] # input data, text
+X = df['text'] # input data, text
 y = df['spam'] # answers, 0 or 1
 # x is like the questions and y is the answers
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
 # text_size is .2 meaning 20% test, 80% train
 
 # confirm sizes of splits
-print(x_train.shape)
-print(x_test.shape)
+print(X_train.shape)
+print(X_test.shape)
 
-vectorizer = TfidfVectorizer()
-x_train_tfidf = vectorizer.fit_transform(x_train)
-x_test_tfidf = vectorizer.transform(x_test)
+# can improve this with parameters
+vectorizer = TfidfVectorizer(stop_words='english', lowercase=True, token_pattern=r'[a-zA-Z]+')
+X_train_tfidf = vectorizer.fit_transform(X_train) # learn vocabulary AND convert to numbers
+X_test_tfidf = vectorizer.transform(X_test) # convert only, vocabulary already learned
 
-print(x_train_tfidf.shape)
+print(X_train_tfidf.shape)
 
-model = MultinomialNB()
-model.fit(x_train_tfidf, y_train)
+model = LogisticRegression()
 
-y_pred = model.predict(x_test_tfidf)
+# model.fit trains the model, looks at TF-IDF numbers alongide the correct labels
+# and learns the patterns that seperate spam vs legit
+model.fit(X_train_tfidf, y_train)
 
+y_pred = model.predict(X_test_tfidf)
+
+# Reading Classification Report
+# Columns: precision (correct when flagged) | recall (caught of all actual) | f1-score (balance of both) | support (actual count in test set)
+# Row 0 (legit) : how well model handles legit emails
+# Row 1 (spam) : how well model handles spam emails
+# Accuracy : overall but misleading due to imbalanced dataset
+# PRINTS BELOW
 print(metrics.classification_report(y_test, y_pred))
+
